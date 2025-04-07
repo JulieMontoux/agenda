0a5473212f3ccbb284866be313fcfa27126c0e00
Julie Montoux <92477225+JulieMontoux@users.noreply.github.com>
2025-04-07 08:41:28 +0000

    FIX add Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Router } from 'react-router-dom';
 import CalendarAgenda from './components/CalendarAgenda';
 import ListeDevoirs from './pages/ListeDevoirs';
 import AjouterDevoir from './pages/AjouterDevoir';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<CalendarAgenda />} />
-      <Route path="/liste" element={<ListeDevoirs />} />
-      <Route path="/ajouter" element={<AjouterDevoir />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<CalendarAgenda />} />
+        <Route path="/liste" element={<ListeDevoirs />} />
+        <Route path="/ajouter" element={<AjouterDevoir />} />
+      </Routes>
+    </Router>
   );
 }
 
